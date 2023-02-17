5bbfb82c3d1c2ad609bdc617df86fc38c3b0b6a8
Holly Phung <hollyphung95@gmail.com>
2023-02-17 06:35:12 +0000

    Added a function renderEntry w/ a single param entry. this param represents a single obj.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@ var $entryForm = document.querySelector('#entry-form');
 var $entryImg = $entryForm.querySelector('img');
 
 var $photoUrl = document.querySelector('#photoUrl');
+
+const entries = JSON.parse(localStorage.getItem('entries'));
+if (entries) {
+  entries.forEach(entry => renderEntry(entry));
+}
+
 $photoUrl.addEventListener('input', function (event) {
   $entryImg.setAttribute('src', event.target.value);
 });
 
-var $saveEntry = document.querySelector('#save-entry');
+var $saveEntry = document.querySelector('.save-entry');
 $saveEntry.addEventListener('submit', saveBtnClick);
 
 function saveBtnClick(event) {
@@ -21,4 +27,41 @@ function saveBtnClick(event) {
   data.entries.unshift(entry);
   $entryImg.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
+}
+
+var ul = document.querySelector('ul');
+
+function renderEntry(entry) {
+  var li = document.createElement('li');
+  ul.appendChild(li);
+
+  var container = document.createElement('div');
+  container.classList.add('container');
+  li.appendChild(container);
+
+  var row = document.createElement('div');
+  row.classList.add('row');
+  container.appendChild(row);
+
+  var column = document.createElement('div');
+  column.classList.add('column-half');
+  row.appendChild(column);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('src', entry.getAttribute('photoUrl'));
+  column.appendChild($img);
+
+  column = document.createElement('div');
+  column.classList.add('column-half');
+  row.appendChild(column);
+
+  var $title = document.createElement('h3');
+  $title.setAttribute('class', entry.getAttribute('name'));
+  column.appendChild($title);
+
+  var $notes = document.createElement('p');
+  $notes.setAttribute('class', entry.getAttribute('notes'));
+  column.appendChild($notes);
+
+  ul.appendChild(li);
 }
